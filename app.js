6cded8a6d5ed94b05a3ec73b512f8fa7c6c62b02
Priya K Gowda <83060746+PriyaKGowda@users.noreply.github.com>
2021-09-29 11:07:05 +0530

    added text-input

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("script is working from a different file")
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+
+console.log(textInput);
+console.log(btnTranslate);
+
+function clickHandler() 
+{
+        console.log("clicked");
+        console.log("text",textInput);
+};
+
+btnTranslate.addEventListener("click",clickHandler);
