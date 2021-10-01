fb76c9283a36c2ab320c7f59fa02f2401fadbc0a
Priya K Gowda <83060746+PriyaKGowda@users.noreply.github.com>
2021-10-01 19:11:55 +0530

    post man is done

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+function getTranslationURL(text) {
+   return serverURL + "?" + "text=" + text  
+   
+}
 
 function clickHandler() 
 {
-   outputDiv.innertext = "Is wash ter gass"+ textInput.value ;    
+   var inputText = textInpu.value;  
+   
+   fetch(getTranslationURL(inputText))
+      .then((response => response.json())
+      .then(json => console.log(json.contents.translated))
 };
 
 btnTranslate.addEventListener("click",clickHandler);
